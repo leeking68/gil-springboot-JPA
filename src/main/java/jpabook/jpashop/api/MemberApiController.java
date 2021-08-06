e0d377d86621045c078aa51229281f36d8fff2f5
gilhoLee <leeeeeegilho@gmail.com>
2021-08-07 01:10:36 +0900

    add member api put get

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -8,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,13 +17,39 @@ public class MemberApiController {
 
     private final MemberService memberService;
 
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result memberV2() {
+        List<Member>  findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream().map(m->new MemberDto(m.getName())).collect(Collectors.toList());
+        return  new Result(collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private  T  data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
+    }
+
+
+
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
         Long id = memberService.join(member);
         return  new CreateMemberResponse(id);
     }
 
-    @PostMapping("/ap1/v2/members")
+    @PostMapping("/api/v2/members")
     public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
         Member member = new Member();
         member.setName(request.getName());
