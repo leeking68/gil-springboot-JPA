4fb9dec27b49be4ade541f0c1a02be5ed95cb57a
gilhoLee <leeeeeegilho@gmail.com>
2021-08-13 23:53:39 +0900

    modify code

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -25,11 +25,11 @@ public class OrderSimpleApiController {
 /**
  * V1. 엔티티 직접 노출
  * - Hibernate5Module 모듈 등록, LAZY=null 처리
- * - 양방향 관계 문제 발생 -> @JsonIgnore 
+ * - 양방향 관계 문제 발생 -> @JsonIgnore
  */
 @GetMapping("/api/v1/simple-orders")
 public List<Order> ordersV1() {
-    List<Order> all = orderRepository.findAll();
+    List<Order> all = orderRepository.findAllByString(new OrderSearch());
     for (Order order : all) {
         order.getMember().getName(); //Lazy 강제 초기화
         order.getDelivery().getAddress(); //Lazy 강제 초기환
